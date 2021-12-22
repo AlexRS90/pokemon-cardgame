26b6fde6102ac92789a68d36657c8966441abf2b
AlexRS90 <aramosbengy@gmail.com>
2021-12-22 14:17:07 -0600

    Add rountes to both components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import React from 'react';
+import { Routes, Route } from 'react-router';
+import { useLocation } from 'react-router';
+import ShowPokemon from './components/homePageBottom';
+import DetailInfo from './components/details';
 
-const App = () => (
-  <p>Hello world</p>
+const App = () => {
+  let location = useLocation();
+  return (
+  <>
+    <Routes>
+      <Route path="/" element={<ShowPokemon />} />
+      <Route path="/detail/:id" element={<DetailInfo data={location.state} />} />
+    </Routes>
+  </>
 );
+  }
 
 export default App;
