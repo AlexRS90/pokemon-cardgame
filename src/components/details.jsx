e14ea9424570b99472f52e310880744c501edf9a
AlexRS90 <aramosbengy@gmail.com>
2021-12-22 14:06:21 -0600

    Add details component

--- src/components/details.jsx
+++ src/components/details.jsx
@@ -1,22 +1,56 @@
 import React from 'react';
+import Navbar from './navbar';
+import './detail.css';
 
 const DetailInfo = (props) => {
-    console.log(props.data);
     const { id, name, pokedex, hp, type, attack, rarity, image, price } = props.data;
     return (
-      <>
-        <section>
-          <div>
+      <div key={id}>
+        <Navbar name={name} />
+        <section className="top-container">
+          <div className="top-wrapper">
             <img src={image} alt="Pokemon" />
-            <p>{name}</p>
-            <p>Pokedex Number: {pokedex}</p>
+            <div>
+              <p className="top-name">{name}</p>
+              <p className="top-pokedex">Pokedex Number: {pokedex}</p>
+            </div>
           </div>
-          <p>Hit Power: {hp}</p>
+          <p className="middle-bar">Hit Power: {hp}</p>
         </section>
-        <section>
-          
+        <section className="details-container">
+          <div className="d-flex bg-color">
+            <p>Type</p>
+            <p>{type}</p>
+          </div>
+          <div className="attack-wrapper bg-color">
+            <p className="title-table">List of Attacks</p>
+            {attack.map((e) => (
+              <div className="d-flex">
+                <p>Attack</p>
+                <p>{e.name}</p>
+              </div>
+            ))}
+          </div>
+          <div className="d-flex bg-color">
+            <p>Rarity</p>
+            <p>{rarity}</p>
+          </div>
+          <div className="price-wrapper bg-color">
+            <p className="title-table">Card Price in USD</p>           
+            <div className="d-flex">
+              <p>Average</p>
+              <p>$ {price.averageSellPrice}</p>
+            </div><div className="d-flex">
+              <p>Lowest</p>
+              <p>$ {price.lowPrice}</p>
+            </div>
+            <div className="d-flex">
+              <p>Trend</p>
+              <p>$ {price.trendPrice}</p>
+            </div>
+          </div>
         </section>
-      </>
+      </div>
     );
 
 };
